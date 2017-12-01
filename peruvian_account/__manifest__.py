1c3f50345ee8a0f5ae7577eb695fd4454c77243c
Christian Horna <christian1020@gmail.com>
2017-12-01 17:07:46 -0500

    add ruc to partners

---
+++ peruvian_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Peruvian Account',
+    'author': 'chorna',
+    'version': '1.0',
+    'category': 'Account',
+    'description': """
+        Contabilidad Peruana
+    """,
+    'depends': ['account',],
+    'data': [
+        'views/partner.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
